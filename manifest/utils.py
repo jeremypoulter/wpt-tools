a96ea2b5e0fa032ce2d1929bd9bebfb940d12eb3
James Graham <james@hoppipolla.co.uk>
2016-09-27 16:14:11 +0100

    Change the manifest format.
    The redesign has several goals:
    
    * Make noop changes as fast as possible, including in cases where the
      tests are vendored into third-party repositories, so that the manifest
      can be updated before every test run without imposing a substantial
      performance burden.
    
    * Make it possible for tools such a the lint to only operate on files
      that have changed since the last run, thus providing a considerable
      performance improvement to those tools.
    
    The general approach taken is to hash every file in the repository and
    store that hash in the manifest. Then expensive operations such as
    parsing the file are only performed when the hash has changed. The
    desire to use the manifest as input to the lint means that every
    lintable file, not just tests, has to be included.
    
    On my machine this read+hash process takes about 10% of the total
    execution time, so there is clearly some performance left on the
    table. In particular:
    
    * The gitignore implementation is not very performant (this also takes
      about 10% of the runtime).
    
    * Constructing objects for each test can be slow, and could be done
      lazilly.
    
    * Reading a large JSON file is intrinsically slow.
    
    * Python itself doesn't fully optimise things like walking directories
      or regexps, which leaves some performance on the table in other areas.
    
    This list is not intended to be exhaustive.

--- manifest/utils.py
+++ manifest/utils.py
@@ -1,9 +1,6 @@
 import os
 from six import BytesIO
 
-blacklist = ["/tools/", "/resources/", "/common/", "/conformance-checkers/", "/_certs/"]
-blacklist_in = ["/resources/", "/support/"]
-
 def rel_path_to_url(rel_path, url_base="/"):
     assert not os.path.isabs(rel_path)
     if url_base[0] != "/":
@@ -12,22 +9,14 @@ def rel_path_to_url(rel_path, url_base="/"):
         url_base += "/"
     return url_base + rel_path.replace(os.sep, "/")
 
-def is_blacklisted(url):
-    if "/" not in url[1:]:
-        return True
-    for item in blacklist:
-        if url.startswith(item):
-            return True
-    for item in blacklist_in:
-        if item in url:
-            return True
-    return False
 
 def from_os_path(path):
     return path.replace(os.path.sep, "/")
 
+
 def to_os_path(path):
     return path.replace("/", os.path.sep)
+
 
 class ContextManagerBytesIO(BytesIO):
     def __enter__(self):
@@ -35,6 +24,7 @@ class ContextManagerBytesIO(BytesIO):
 
     def __exit__(self, *args, **kwargs):
         self.close()
+
 
 class cached_property(object):
     def __init__(self, func):
